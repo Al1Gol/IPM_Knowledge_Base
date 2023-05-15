83f3df9be92d0cdc09a7f038e62726d8483e0188
KorepanovA <al1working@mail.ru>
2023-05-15 13:12:40 +0300

    Роутеры перемещены в mainapp

--- ipm_knowledge/mainapp/urls.py
+++ ipm_knowledge/mainapp/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 from rest_framework import routers
 from mainapp.views import SrvReleasesViewSet, PlcReleasesViewSet, ReleaseChangesViewSet
 
+router = routers.DefaultRouter()
+router.register('srv_releases', SrvReleasesViewSet, basename='srv_releases')
+router.register('plc_releases', PlcReleasesViewSet, basename='plc_releases')
+router.register('release_changes', ReleaseChangesViewSet, basename='release_changes')
+
+
 urlpatterns = [
-    
+    path('api/v1/', include(router.urls)),
 ]
