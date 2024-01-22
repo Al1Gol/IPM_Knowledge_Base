9bad5f615aab97f713c3ad2a24f784c2ed44278a
KorepanovA <al1working@mail.ru>
2024-01-22 15:03:10 +0300

    Настроены выравнивание названий разделов и иконок разделов

--- frontend/src/components/Sections.js
+++ frontend/src/components/Sections.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import add_img from '../img/icons/add_section.svg'
+import editIcon from '../img/icons/edit_sub.png'
 
 
 /*-----------------------------------*/
@@ -10,9 +11,14 @@ import add_img from '../img/icons/add_section.svg'
 /*-----------------------------------*/
 const SectionItem = ({section}) => {
     return (
-        <div className='sectionBtn'>
-            <button> <img src={section.img ? section.img : ''} alt=''></img> {section.name} </button>
-            <button>edit</button>
+        <div className='sectionItem'>
+            <div className='sectionBtn'>
+                <div>
+                    {section.img ? <img src= {section.img} alt=''></img>  : ''}
+                </div>
+                <p>{section.name}</p>
+                <img className='iconSection' src={editIcon}></img>
+            </div>
         </div>
     )
 }
@@ -27,7 +33,9 @@ const Sections = ({ sections, onFormDisplay }) => {
         <div className='sectionsBlock'>
             {sections.map((section) => <SectionItem section={section} />)}
             <div id="sectionAdd" className='sectionBtn' onClick = {(event) => onFormDisplay(event.currentTarget.getAttribute("id"))}> 
-                <img src={add_img} alt='+'></img>
+                <div>
+                    <img src={add_img} alt='+'></img>
+                </div>
                 <p>Добавить раздел</p></div>
         </div>
     )
