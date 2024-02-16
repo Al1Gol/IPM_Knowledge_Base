49c508d47acc8aa4ce63ebbb9b737a680b29f945
KorepanovA <al1working@mail.ru>
2024-02-16 13:24:36 +0300

    Добавлено условие для режима дебага на бэкенде. При отсутствии переменной окружения быдет включаться режим DEBUG

--- backend/ipm_knowledge/ipm_knowledge/settings.py
+++ backend/ipm_knowledge/ipm_knowledge/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h@ik9e0(lv3+j#@uz+*rt*c6ysz!zd)wftn!-ji620h-=^bn@!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv("DJNAGO_PRODUCTION", default=None) else True
 
 ALLOWED_HOSTS = ["*"]
 
