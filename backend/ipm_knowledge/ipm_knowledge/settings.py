e0d37befae3e47a4603a4870cb75d61e09166e6c
KorepanovA <al1working@mail.ru>
2024-03-26 08:55:33 +0300

    Добавлена конфигрурация по условию на подключение к БД для дебаг и прод режимов

--- backend/ipm_knowledge/ipm_knowledge/settings.py
+++ backend/ipm_knowledge/ipm_knowledge/settings.py
@@ -106,17 +106,28 @@ WSGI_APPLICATION = "ipm_knowledge.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ipm_knowledge",
-        "USER": "postgres",
-        "PASSWORD": "123",
-        "HOST": "localhost",
-        "PORT": "5432",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "ipm_knowledge",
+            "USER": "postgres",
+            "PASSWORD": "123",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "ipm_knowledge",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "db",
+            "PORT": "5432",
+        }
+    }
 
 
 # Users model
