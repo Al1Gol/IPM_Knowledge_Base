a8687908baf46f9a4435f0518ea5fb2ddc3f0a4d
KorepanovA <al1working@mail.ru>
2024-03-30 23:24:01 +0300

    Добавлен и настроен докер для обработки фронтенда

--- backend/ipm_knowledge/ipm_knowledge/settings/production.py
+++ backend/ipm_knowledge/ipm_knowledge/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ipm_knowledge",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "Xrsw%JD!06rkP?]b0GHYuk):_XFJ*,UZiq_",
         "HOST": "db",
         "PORT": "5432",
     }
